a26a4d474d4d3817f8e6c046f0698431950c11db
nhantran1711 <traty191@mymail.unisa.edu.au>
2026-07-02 17:54:21 +0930

    Added both genereatad image and optimzer using Adam

--- nst/main.py
+++ nst/main.py
@@ -17,10 +17,17 @@ style_image = load_image("images/style.jpg").to(device)
 # Create an instance of the VGGFeatures model, move it to the device, and set it to evaluation mode
 model = VGGFeatures().to(device).eval()
 
-# Pass the content
+# Pass the content and style images through the model
 content_target, style_target = model(content_image)
 _, style_feature = model(style_image)
 
 # Define the content and style loss functions
 content_loss = ContentLoss(content_target)
 style_loss = [StyleLoss(style_feature[i]) for i in range(len(style_feature))]
+
+# Init generated image as a clone of the content image and set requires_grad to True for optimization
+generated_image = content_image.clone().requires_grad_(True)
+
+# Define the optimizer for the generated image using Adam optimizer with a learning rate of 0.01
+optimizer = optim.Adam([generated_image], lr=0.01)
+
