33993cce91a30bcf96e5c094c2d3edb8366827a8
nhantran1711 <traty191@mymail.unisa.edu.au>
2026-07-02 17:49:18 +0930

    Define the content and style

--- nst/main.py
+++ nst/main.py
@@ -14,3 +14,13 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 content_image = load_image("images/content.jpg").to(device)
 style_image = load_image("images/style.jpg").to(device)
 
+# Create an instance of the VGGFeatures model, move it to the device, and set it to evaluation mode
+model = VGGFeatures().to(device).eval()
+
+# Pass the content
+content_target, style_target = model(content_image)
+_, style_feature = model(style_image)
+
+# Define the content and style loss functions
+content_loss = ContentLoss(content_target)
+style_loss = [StyleLoss(style_feature[i]) for i in range(len(style_feature))]
